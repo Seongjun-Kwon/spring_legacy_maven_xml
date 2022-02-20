2d845c01c55736e1d0bf900ba389bb96ef5bce6e
Kwon seong jun <tjdwns1865@naver.com>
2022-02-21 08:32:44 +0900

    jdbc test

---
+++ src/test/java/com/tistory/modaljoa/persistence/JdbcTests.java
@@ -0,0 +1,36 @@
+package com.tistory.modaljoa.persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.fail;
+
+@Log4j
+public class JdbcTests {
+
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection() {
+
+        try (Connection conn =
+                     DriverManager.getConnection(
+                             "jdbc:oracle:thin:@localhost:1521:XE",
+                             "SPRING",
+                             "1234")) {
+
+            log.info(conn);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
