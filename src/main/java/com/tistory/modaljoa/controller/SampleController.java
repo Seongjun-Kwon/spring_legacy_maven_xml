5f41a37600a966cad3ff35e6cd15ed3d96ee3a5f
Kwon seong jun <tjdwns1865@naver.com>
2022-02-26 07:53:33 +0900

    Exception 처리 실습

--- src/main/java/com/tistory/modaljoa/controller/SampleController.java
+++ src/main/java/com/tistory/modaljoa/controller/SampleController.java
@@ -4,6 +4,7 @@ import com.tistory.modaljoa.domain.SampleDTO;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,5 +30,14 @@ public class SampleController {
         log.info("/exUpload-------------------------");
 
         return "sample/exUpload";
+    }
+
+    @GetMapping("/exParam")
+    public String exParam(SampleDTO dto, @ModelAttribute("page") int page) {
+
+        log.info("dto:" + dto);
+        log.info("page:" + page);
+
+        return "sample/exParam";
     }
 }
