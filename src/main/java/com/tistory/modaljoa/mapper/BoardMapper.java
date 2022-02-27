bceaafc45ef28c225cdcb1451c95fb25d7120824
Kwon seong jun <tjdwns1865@naver.com>
2022-02-28 01:53:38 +0900

    DB layer CRUD 구현, test

--- src/main/java/com/tistory/modaljoa/mapper/BoardMapper.java
+++ src/main/java/com/tistory/modaljoa/mapper/BoardMapper.java
@@ -7,6 +7,16 @@ import java.util.List;
 
 public interface BoardMapper {
 
-//    @Select("SELECT * FROM tbl_board WHERE bno > 0")
+    //    @Select("SELECT * FROM tbl_board WHERE bno > 0")
     public List<BoardVO> getList();
+
+    public void insert(BoardVO board);
+
+    public void insertSelectKey(BoardVO board);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long bno);
+
+    public int update(BoardVO board);
 }
--- src/test/java/com/tistory/modaljoa/mapper/BoardMapperTests.java
+++ src/test/java/com/tistory/modaljoa/mapper/BoardMapperTests.java
@@ -24,4 +24,57 @@ public class BoardMapperTests {
             log.info(board);
         }
     }
+
+    @Test
+    public void testInsert() {
+
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 작성하는 글");
+        board.setContent("새로 작성하는 내용");
+        board.setWriter("newbie");
+
+        mapper.insert(board);
+
+        log.info(board);
+    }
+
+    @Test
+    public void testInsertSelectKey() {
+
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 작성하는 글 select key");
+        board.setContent("새로 작성하는 내용 select key");
+        board.setWriter("newbie");
+
+        mapper.insertSelectKey(board);
+
+        log.info(board);
+    }
+
+    @Test
+    public void testRead() {
+
+        BoardVO board = mapper.read(5L);
+
+        log.info(board);
+    }
+
+    @Test
+    public void testDelete() {
+
+        log.info("DELETE COUNT: " + mapper.delete(5L));
+    }
+
+    @Test
+    public void testUpdate() {
+
+        BoardVO board = new BoardVO();
+
+        board.setBno(6L);
+        board.setTitle("수정된 제목");
+        board.setContent("수정된 내용");
+        board.setWriter("user00");
+
+        log.info("UPDATE COUNT: " + mapper.update(board));
+    }
 }
