42b5cabcec544c70e810ca19f54bcde3767d1a47
Kwon seong jun <tjdwns1865@naver.com>
2022-02-28 14:19:14 +0900

    Business layer 구현, test

---
+++ src/main/java/com/tistory/modaljoa/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.tistory.modaljoa.service;
+
+import com.tistory.modaljoa.domain.BoardVO;
+
+import java.util.List;
+
+public interface BoardService {
+
+    public void register(BoardVO board);
+
+    public BoardVO get(Long bno);
+
+    public boolean modify(BoardVO board);
+
+    public boolean remove(Long bno);
+
+    public List<BoardVO> getList();
+}
