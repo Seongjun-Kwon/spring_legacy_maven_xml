3c36908c5fb507f5b6f88f51c52af0a9d6c61c51
Kwon seong jun <tjdwns1865@naver.com>
2022-03-14 05:22:17 +0900

    Ajax 댓글 처리 Business layer 구현

---
+++ src/main/java/com/tistory/modaljoa/service/ReplyService.java
@@ -0,0 +1,20 @@
+package com.tistory.modaljoa.service;
+
+import com.tistory.modaljoa.domain.Criteria;
+import com.tistory.modaljoa.domain.ReplyVO;
+
+import java.util.List;
+
+public interface ReplyService {
+
+    public int register(ReplyVO reply);
+
+    public ReplyVO get(Long rno);
+
+    public int remove(Long rno);
+
+    public int modify(ReplyVO reply);
+
+    public List<ReplyVO> getList(Criteria cri, Long bno);
+
+}
---
+++ src/main/java/com/tistory/modaljoa/service/ReplyServiceImpl.java
@@ -0,0 +1,58 @@
+package com.tistory.modaljoa.service;
+
+import com.tistory.modaljoa.domain.Criteria;
+import com.tistory.modaljoa.domain.ReplyVO;
+import com.tistory.modaljoa.mapper.ReplyMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Log4j
+public class ReplyServiceImpl implements ReplyService {
+
+    private ReplyMapper mapper;
+
+    @Override
+    public int register(ReplyVO reply) {
+
+        log.info("register: " + reply);
+
+        return mapper.insert(reply);
+    }
+
+    @Override
+    public ReplyVO get(Long rno) {
+
+        log.info("get: " + rno);
+
+        return mapper.read(rno);
+    }
+
+    @Override
+    public int remove(Long rno) {
+
+        log.info("remove: " + rno);
+
+        return mapper.delete(rno);
+    }
+
+    @Override
+    public int modify(ReplyVO reply) {
+
+        log.info("modify: " + reply);
+
+        return mapper.update(reply);
+    }
+
+    @Override
+    public List<ReplyVO> getList(Criteria cri, Long bno) {
+
+        log.info("get List: " + bno);
+
+        return mapper.getListWithPaging(cri, bno);
+    }
+}
